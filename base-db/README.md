39f1d419a006349a056c4957ec5b8a6da81c1be7
Srirag Nair <ksriragnair@gmail.com>
2017-03-11 12:49:26 +0530

    Database Update

--- base-db/README.md
+++ base-db/README.md
@@ -1,3 +1,8 @@
 "# sails-codeathon"
 
 Base DB structure scripts
+
+User Account:
+Host: localhost
+Username: sails_db
+Password: Sails1119
---
+++ base-db/sails-catdb.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 11, 2017 at 07:17 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sails-catdb`
+--
+CREATE DATABASE IF NOT EXISTS `sails-catdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `sails-catdb`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sc_users`
+--
+
+DROP TABLE IF EXISTS `sc_users`;
+CREATE TABLE IF NOT EXISTS `sc_users` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sc_users`
+--
+
+INSERT INTO `sc_users` (`ID`, `FirstName`, `LastName`, `Username`, `Password`) VALUES
+(1, 'Srirag', 'Nair', 'srirag', 'sails@123'),
+(2, 'Pavan', 'Kumar', 'pavan', 'sails@123'),
+(3, 'Kiran', 'AVK', 'avkkiran', 'sails@123'),
+(4, 'Kiran', 'Sangita', 'kiran', 'sails@123');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
