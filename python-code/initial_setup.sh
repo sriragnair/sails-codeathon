537a6f54dda175e1dfcb1f034f61dfe0a31914cf
Pavan Kumar Aryasomayajulu <pavan@sails.solutions>
2017-03-14 12:38:53 +0530

    Update initial_setup.sh

--- python-code/initial_setup.sh
+++ python-code/initial_setup.sh
@@ -1,4 +1,5 @@
 sudo apt-get install python-mysqldb
+sudo apt install python-pip
 sudo pip install --upgrade google-api-python-client
 sudo apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev
 sudo pip install pyopenssl
